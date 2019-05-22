d420b7e10cc5c0359ae05d4bb7ca3132d78879bf
Tiffany J Brown <tiffany.brownjc@gmail.com>
2019-05-22 16:03:47 -0400

    added project links photos not displayed

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -7,38 +7,26 @@ class Projects extends Component {
       <div>
         <h1>Projects</h1>
         <div className="recipe-game">
-          <a
-            className="project-link"
-            href="https://pages.git.generalassemb.ly/TJBrown2/dish-name-game/"
-          > 
-            <div className="image"></div>
+          <a className="project-link" href="https://pages.git.generalassemb.ly/TJBrown2/dish-name-game/"> 
+            <div className="image"><img src="src/images/Project-1.png" alt="" ></img></div>
             Recipe Game
           </a>
         </div>
         <div className="med-cabinet">
-          <a
-            className="project-link"
-            href="https://tiffanys-medicine-cabinet.herokuapp.com/"
-          >
-          <div className="image"></div>
+          <a className="project-link" href="https://tiffanys-medicine-cabinet.herokuapp.com/">
+          <div className="image"><img src="src/images/Project-2.png" alt="" ></img></div>
             Medicine Cabinet
           </a>
         </div>
         <div className="rogue-gallery">
-          <a
-            className="project-link"
-            href="https://rogues-gallery.herokuapp.com/"
-          >
-          <div className="image"><img src="https://imgur.com/AyRJfov" alt="" ></img></div>
+          <a className="project-link" href="https://rogues-gallery.herokuapp.com/">
+          <div className="image"><img src="src/images/Project-3.png" alt="" ></img></div>
             Rogue Gallery
           </a>
         </div>
         <div className="allergy-free">
-          <a
-            className="project-link"
-            href="https://nyc-allergy-free-eats.herokuapp.com/"
-          >
-          <div className="image"></div>
+          <a className="project-link" href="https://nyc-allergy-free-eats.herokuapp.com/">
+          <div className="image"><img src="src/images/Project-4.png" alt="" ></img></div>
             Allergy Free Eats
           </a>
         </div>
