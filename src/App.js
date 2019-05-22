28c2d6a2204f908a10e5916f6458c73068d50706
Tiffany J Brown <tiffany.brownjc@gmail.com>
2019-05-22 16:32:48 -0400

    added resume component and scss, route/ link

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Sidebar from "./Components/Sidebar";
 import Introduction from "./Components/Introduction";
 import About from "./Components/About";
 import Projects from "./Components/Projects";
+import Resume from "./Components/Resume";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
@@ -18,12 +19,14 @@ class App extends Component {
                 <Link className="link" to="/">Home</Link>
                 <Link className="link" to="/about">About</Link>
                 <Link className="link" to="/projects">Projects</Link>
+                <Link className="link" to="/resume">Resume</Link>
               </nav>
               <Sidebar />
               <div className="content">
               <Route exact path="/" component={Introduction} />
               <Route exact path="/about" component={About} />
               <Route exact path="/projects" component={Projects} />
+              <Route exact path="/resume" component={Resume} />
               </div>
             </div>
           
---
+++ src/Components/Resume.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Resume.scss";
+
+class Resume extends Component {
+    render() {
+      return (
+        <div>
+          
+        </div>
+      );
+    }
+  }
+  export default Resume;
+  
