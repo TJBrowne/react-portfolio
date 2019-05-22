05fddce70cc7ad685dfc4a26383122e4f370ebcc
Tiffany J Brown <tiffany.brownjc@gmail.com>
2019-05-22 17:15:09 -0400

    more scss on intro

--- src/Components/Introduction.js
+++ src/Components/Introduction.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./Introduction.scss";
 
 class Introduction extends Component {
-    render() {
-      return (
-        <div className="Intro">
-        {/* <span></span> */}
-            <h1>Tiffany Brown</h1>
-            <h3>Web Developer</h3>
+  render() {
+    return (
+      <main>
+        <div className="intro">
+          <h1>Tiffany Brown</h1>
+          <h3>Full-Stack Web Developer</h3>
         </div>
-      )
-    }
+      </main>
+    );
   }
-  export default Introduction;
+}
+export default Introduction;
