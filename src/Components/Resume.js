7523268d5bf4cc8e45c90a2c9c0e89270170802e
Tiffany J Brown <tiffany.brownjc@gmail.com>
2019-05-31 22:32:37 -0400

    playing around with resume link and routes

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,11 +1,27 @@
 import React, { Component } from "react";
+import { Redirect } from 'react-router-dom'
 import "./Resume.scss";
 
 class Resume extends Component {
+  state = {
+    redirect: false
+  }
+  setRedirect = () => {
+    this.setState({
+      redirect: true
+    })
+  }
+  renderRedirect = () => {
+    if (this.state.redirect) {
+      return <Redirect to="https://resume.creddle.io/resume/4jjqqhhtuf3" />
+    }
+  }
+
     render() {
       return (
         <div>
-          
+          {this.renderRedirect()}
+        <button onClick={this.setRedirect}>Resume</button>
         </div>
       );
     }
